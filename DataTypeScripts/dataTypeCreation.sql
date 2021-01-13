71ca66db2405e43cf80838b84b420104d4c3a865
Cade Parkhurst <parkhuca@rose-hulman.edu>
2021-01-13 17:28:13 -0500

    data type creation

---
+++ DataTypeScripts/dataTypeCreation.sql
@@ -0,0 +1,14 @@
+CREATE TYPE languageName
+FROM varchar(20) NOT NULL
+
+CREATE TYPE weaponTypeName
+FROM varchar(30)
+
+CREATE TYPE armorWeightName
+FROM varchar(30)
+
+CREATE TYPE raceName
+FROM varchar(30)
+
+CREATE TYPE backgroundName
+FROM varchar(30)
