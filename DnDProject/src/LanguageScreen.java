8fb477d66e3c3157f56ad9547df4541928c7156a
Andrew Orians <oriansaj@rose-hulman.edu>
2021-02-17 23:52:27 -0500

    Made lang screen look nicer

--- DnDProject/src/LanguageScreen.java
+++ DnDProject/src/LanguageScreen.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,13 +20,12 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-
-
-public class LanguageScreen extends JPanel{
+public class LanguageScreen extends JPanel {
 	private PageManager manager;
 	private ConnectionHelper loginHelper;
 	private ResultSet langs;
@@ -35,68 +35,77 @@ public class LanguageScreen extends JPanel{
 	private JTable langTable;
 	private String characterID;
 	private JButton backButton;
-	
-	public LanguageScreen(PageManager manager) { //This screen shows available characters
+	private JPanel optionsPanel;
+	private JTextField langName;
+	private JScrollPane langScrollTable;
+
+	public LanguageScreen(PageManager manager) { // This screen shows available characters
 		this.manager = manager;
-		
+
 		this.newLangButton = new JButton("New Language");
-		
+		this.optionsPanel = new JPanel();
+		this.optionsPanel.setLayout(new GridLayout(0, 3));
 		this.characterID = "1";
-		
+		this.setLayout(new BorderLayout());
+		this.langName = new JTextField();
+		this.optionsPanel.add(langName);
 		newLangButton.addActionListener(new newLangButtonListener());
+		this.optionsPanel.add(newLangButton);
 		this.backButton = new JButton("Back To Stats");
 		backButton.addActionListener(new backListener());
-		this.add(backButton);
-		
+		this.optionsPanel.add(backButton);
+		this.add(this.optionsPanel, BorderLayout.SOUTH);
+
 //		this.add(characterField).setLocation(2, 0);
 //		this.add(newItemButton).setLocation(0, 0);
-		//hide();
-		
-		
-		
+		// hide();
 
-		
 	}
+
 	public void updateForCharacter() {
 		this.characterID = manager.getCharacterChosen();
 		System.out.println(this.characterID);
 		this.langs = this.getLangNames();
 //		int rows = this.items.size() > 10? 10:this.items.size();
-		this.layout = new GridLayout(2,3);
-		this.setLayout(layout);
+//		this.layout = new GridLayout(2,3);
+//		this.setLayout(layout);
 		if (this.langTable != null) {
 			this.remove(langTable);
 		}
 		try {
 			this.langTable = new JTable(buildTableModel(this.langs));
-			this.add(this.langTable).setLocation(2, 2);;
+			this.langScrollTable = new JScrollPane(this.langTable);
+			Dimension size = this.langScrollTable.getPreferredSize();
+			size.setSize(size.width + 1000, size.height);
+			this.langScrollTable.setPreferredSize(size);
+			this.add(this.langScrollTable, BorderLayout.NORTH);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	public ResultSet getLangNames(){
+
+	public ResultSet getLangNames() {
 		try {
 			CallableStatement cstmt = this.manager.getConnection().prepareCall("{call getLanguages(?)}");
 			cstmt.setInt(1, Integer.valueOf(this.characterID));
-			
+
 			ResultSet rs = cstmt.executeQuery();
-			//System.out.println(rs);
+			// System.out.println(rs);
 //			System.out.println(rs);
 //			while (rs.next()) {
 //				names.put(rs.getString(1), rs.getString(2));
 //			}
-			//System.out.println(names);
+			// System.out.println(names);
 			return rs;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
+
 //	private HashMap<JButton, String> generateButtons() {
 //		HashMap<JButton,String> buttons = new HashMap<>();
 //		
@@ -112,49 +121,46 @@ public class LanguageScreen extends JPanel{
 //		}
 //		return buttons;
 //	}
-	
-	
+
 	class newLangButtonListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Switch to new char action listen");
 			manager.switchPage("AddCharacter");
-		}	
+		}
 	}
-	
-	
-	// Taken From https://stackoverflow.com/questions/10620448/most-simple-code-to-populate-jtable-from-resultset
-	public static DefaultTableModel buildTableModel(ResultSet rs)
-	        throws SQLException {
-	    ResultSetMetaData metaData = rs.getMetaData();
-	    // names of columns
-	    Vector<String> columnNames = new Vector<String>();
-	    int columnCount = metaData.getColumnCount();
-	    for (int column = 1; column <= columnCount; column++) {
-	        columnNames.add(metaData.getColumnName(column));
-	    }
-	    // data of the table
-	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-	    while (rs.next()) {
-	        Vector<Object> vector = new Vector<Object>();
-	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-	            vector.add(rs.getObject(columnIndex));
-	        }
-	        data.add(vector);
-	    }
-	    return new DefaultTableModel(data, columnNames);
+
+	// Taken From
+	// https://stackoverflow.com/questions/10620448/most-simple-code-to-populate-jtable-from-resultset
+	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
+		ResultSetMetaData metaData = rs.getMetaData();
+		// names of columns
+		Vector<String> columnNames = new Vector<String>();
+		int columnCount = metaData.getColumnCount();
+		for (int column = 1; column <= columnCount; column++) {
+			columnNames.add(metaData.getColumnName(column));
+		}
+		// data of the table
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		while (rs.next()) {
+			Vector<Object> vector = new Vector<Object>();
+			for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+				vector.add(rs.getObject(columnIndex));
+			}
+			data.add(vector);
+		}
+		return new DefaultTableModel(data, columnNames);
 	}
-	
-	
+
 	public void toggleState() {
 		setEnabled(!isEnabled());
 	}
-	
+
 	class backListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			manager.switchPage("Details");
-		}	
+		}
 	}
-	
+
 }
